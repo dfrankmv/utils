0979d42fa3124c35187c6b1887a68c0d0626d806
deivit <deivit@mail.com>
2025-10-29 04:43:50 +0100

    wip

--- setup.py
+++ setup.py
@@ -4,30 +4,11 @@ from setuptools import setup, find_packages
 # utils/utils structure #
 # --------------------- #
 
-# setup(
-#     name="utils",
-#     version='0.1.0',
-#     packages=find_packages(exclude=["tests", "tests.*"]),
-#     package_dir={"utils":"utils"},
-#     install_requires=[
-#         "pytest",
-#         "pyzmq",
-#         "requests",
-#         "websocket-client",
-#     ],
-# )
-
-
-# ---------------------- #
-# utils/<code> structure #
-# ---------------------- #
-
 setup(
     name="utils",
     version='0.1.0',
-    packages=["utils"],
-    package_dir={"utils":"."},
-    # packages=find_packages(include=["utils"], exclude=["utils.*"], ),
+    packages=find_packages(),
+    package_dir={"utils":"utils"},
     install_requires=[
         "pytest",
         "pyzmq",
