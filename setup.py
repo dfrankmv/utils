38deafcf53efd3e0033204eba8a361cf7ec6b5c2
deivit <deivit@mail.com>
2025-10-29 03:49:34 +0100

    wip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="utils",
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     package_dir={"utils":"utils"},
     install_requires=[
         "pytest",
